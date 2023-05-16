8e5e9349d4c6d83bb05c4ce05cfee32f8332dccb
Festus Ribiro <festusribiro@gmail.com>
2023-05-16 14:51:36 +0300

    updated renamed controller

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { register } from "./controllers/auth"
+import { userRegistration } from './controllers/auth.js';
 
 /*configurations*/
 const __filename = fileURLToPath(import.meta.url);
@@ -36,7 +36,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /*routes with files*/
-app.post("/auth/register", upload.single("picture"), register)
+app.post("/auth/userRegistration", upload.single("picture"), userRegistration)
     
 /*mongoose setup*/
 const PORT = process.env.PORT || 6001;
