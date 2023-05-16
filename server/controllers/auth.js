0925200789cfab2c08b9b59e81e7c70604da141b
Festus Ribiro <festusribiro@gmail.com>
2023-05-16 16:09:33 +0300

    login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,4 @@
-import bycrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
@@ -16,8 +16,8 @@ export const userRegistration = async (req, res) => {
             occupation
         } = req.body;
 
-        const salt = await bycrypt.genSalt();
-        const passwordHash = await bycrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
 
         const newUser = new User({
             firstName,
@@ -37,4 +37,28 @@ export const userRegistration = async (req, res) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
-}
+};
+
+/*user login*/
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email});
+
+        if (!user) {
+            return res.status(400).json({ msg: "User does not exist."});
+        };
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid Credentials."});
+        };
+
+        const token = jwt.sign({ id:user._id}, process.env.JWT_SECRET);
+        //delete password so that it's not sent to the frontend
+        delete user.password;
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
