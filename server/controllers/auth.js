95bff8b186ee14310c19b564fa3c1a26c38ce1d7
Festus Ribiro <festusribiro@gmail.com>
2023-05-16 14:50:55 +0300

    user registration controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,40 @@
+import bycrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+/*register user*/
+export const userRegistration = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bycrypt.genSalt();
+        const passwordHash = await bycrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        });
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
